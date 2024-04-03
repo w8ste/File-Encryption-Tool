77f1735868d5ee8f6f34a8d2e9da8bcc9c543753
Hannes Albert <hannes10042004@icloud.com>
2024-04-03 03:09:57 +0200

    Implemented the ability to read input from file

--- src/cli/cli.rs
+++ src/cli/cli.rs
@@ -1,4 +1,7 @@
 use clap::Parser;
+use std::fs::File;
+use std::io::{self, Read};
+use clap::builder::Str;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -8,9 +11,27 @@ pub struct Args {
     file: String,
 }
 
-pub fn parse_args() {
+pub fn parse_args() -> String{
     let args = Args::parse();
 
-    println!("The file is called: {}!", args.file)
+    println!("The file is called: {}!", args.file);
+    let contents = match read_file(args.file)  {
+        Ok(contents) => contents,
+        Err(e) => {
+            eprintln!("An error has occurred whilst accessing the file: {}!", e);
+            std::process::exit(1);
+        }
+    };
+
+    contents
+}
+
+fn read_file(file_name : String) -> io::Result<String> {
+    let mut file : File = File::open(file_name)?;
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    Ok(contents)
 }
 
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod cli;
 use crate::cli::cli::parse_args;
 
 fn main() {
-    parse_args()
+    let file: String = parse_args();
+    print!("The contents of the file are:\n{}", file)
 }
 
